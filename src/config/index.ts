b9817bca62b49e653584bef0c4bb3907e4903347
uchiha5555 <innocentdev0512@gmail.com>
2024-01-29 23:15:43 +0800

    fix:server url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 // export const SERVER_URL = "http://localhost:8001/api";
 
 
-export const SERVER_URL = "https://syj3l8-8001.csb.app/api";
+export const SERVER_URL = "https://38vg5z-8001.csb.app/api";
