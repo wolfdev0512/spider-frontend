941f0d3c2f4882c1e6be8f85a05c273803c27112
uchiha5555 <innocentdev0512@gmail.com>
2024-02-12 16:22:04 +0800

    feat: demo

--- src/pages/payment/activate.styles.ts
+++ src/pages/payment/activate.styles.ts
@@ -99,3 +99,81 @@ export const PayButton = styled.button`
   line-height: 22px; /* 137.5% */
   margin-top: 10px;
 `;
+
+export const ModalLayout = styled.div`
+  width: 100%;
+  min-height: 300px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Image = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+export const Single = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const Title = styled.div`
+  font-size: 30px;
+  font-weight: 700;
+  line-height: 2;
+
+  @media screen and (max-width: 425px) {
+    font-size: 25px;
+    font-weight: 700;
+  }
+`;
+export const Container = styled.div`
+  width: 100%;
+
+  border: 1px solid gray;
+  border-radius: 20px;
+  padding: 20px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+export const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const Header = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  @media screen and (max-width: 425px) {
+    font-size: 14px;
+  }
+`;
+export const Detail = styled.div`
+  font-size: 13px;
+  font-weight: 500;
+  span {
+    font-size: 16px;
+    font-weight: 700;
+    margin-right: 5px;
+  }
+  @media screen and (max-width: 425px) {
+    font-size: 9px;
+  }
+`;
+export const Link = styled.a`
+  font-size: 18px;
+  font-weight: 600;
+  text-decoration: underline;
+  color: #69b1ff;
+
+  @media screen and (max-width: 425px) {
+    font-size: 14px;
+  }
+`;
--- src/pages/payment/index.tsx
+++ src/pages/payment/index.tsx
@@ -12,11 +12,15 @@ import { CreditCard, PaymentForm } from "react-square-web-payments-sdk";
 import axios from "axios";
 import { SERVER_URL } from "../../config";
 
+import { Modal } from "antd";
+
 const CheckoutForm: React.FC = () => {
   const router = useNavigate();
   const dispatch = useDispatch();
 
   const [count, setCount] = useState(1);
+  const [openModal, setOpenModal] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const { flag, company, address, name, email, link, size } = useSelector(
     (state: any) => state.single
@@ -36,112 +40,156 @@ const CheckoutForm: React.FC = () => {
   }, []);
 
   return (
-    <form>
-      {!flag ? (
-        <>
-          <Styled.PaySettingWrapper>
-            <div>
-              <h3>Month</h3>
-              <Styled.CounterWrapper>
-                <span
-                  onClick={() =>
-                    setCount((prev) => (prev > 1 ? prev - 1 : prev))
-                  }
-                >
-                  -
-                </span>
-                <input
-                  value={count}
-                  onChange={(e) =>
-                    !isNaN(Number(e.target.value)) &&
-                    Number(e.target.value) > 0 &&
-                    setCount(Number(e.target.value))
-                  }
-                />
-                <span onClick={() => setCount((prev) => prev + 1)}>+</span>
-              </Styled.CounterWrapper>
-            </div>
-            <div>
-              <h3>Price</h3>
-              {/* <h2>${50 * count}.00</h2> */}
-              <h2>${1 * count}.00</h2>
-            </div>
-          </Styled.PaySettingWrapper>
-          <Styled.Divider />
-        </>
-      ) : (<>
-        <Styled.PaySettingWrapper>
-          <div>
-            <h3>Price</h3>
-            <h2>$1</h2>
-          </div>
-        </Styled.PaySettingWrapper>
-        <Styled.Divider />
-      </>)}
-      <PaymentForm
-        applicationId="sq0idp-cy8kSbhWO1avSBHRgeK0IA"
-        locationId="LHBS7S12SPH5C"
-        // applicationId="sandbox-sq0idb-FqQFkG_83JUtftXnrl1Ewg"
-        // locationId="L2VFKV49PTFRN"
-        cardTokenizeResponseReceived={async (token, buyer) => {
-          // let amount = flag ? 8 : count * 50;
-          let amount = flag ? 1 : count * 1;
-          const response = await axios.post(SERVER_URL + "/payment", {
-            sourceId: token.token,
-            amount: amount,
-          });
-          console.log(response)
-          if (response.data.data === "COMPLETED") {
-            if (flag) {
-              const res = await axios.post(SERVER_URL + "/single", {
-                company: company,
-                address: address,
-                name: name,
-                email: email,
-                link: link,
-                size: size,
-              });
-              if (res.data.success) {
-                toast.success(res.data.message)
-                dispatch(
-                  singleActions.setSingle({
-                    flag: false,
-                    company: "",
-                    address: "",
-                    name: "",
-                    email: "",
-                    link: "",
-                    size: "",
-                  })
-                );
-                router("/singlereceipt")
-              }
-              else {
-                toast.error(res.data.message)
+    <>
+      <form>
+        {!flag ? (
+          <>
+            <Styled.PaySettingWrapper>
+              <div>
+                <h3>Month</h3>
+                <Styled.CounterWrapper>
+                  <span
+                    onClick={() =>
+                      setCount((prev) => (prev > 1 ? prev - 1 : prev))
+                    }
+                  >
+                    -
+                  </span>
+                  <input
+                    value={count}
+                    onChange={(e) =>
+                      !isNaN(Number(e.target.value)) &&
+                      Number(e.target.value) > 0 &&
+                      setCount(Number(e.target.value))
+                    }
+                  />
+                  <span onClick={() => setCount((prev) => prev + 1)}>+</span>
+                </Styled.CounterWrapper>
+              </div>
+              <div>
+                <h3>Price</h3>
+                {/* <h2>${50 * count}.00</h2> */}
+                <h2>${1 * count}.00</h2>
+              </div>
+            </Styled.PaySettingWrapper>
+            <Styled.Divider />
+          </>
+        ) : (
+          <>
+            <Styled.PaySettingWrapper>
+              <div>
+                <h3>Price</h3>
+                <h2>$1</h2>
+              </div>
+            </Styled.PaySettingWrapper>
+            <Styled.Divider />
+          </>
+        )}
+        <PaymentForm
+          // applicationId="sq0idp-cy8kSbhWO1avSBHRgeK0IA"
+          // locationId="LHBS7S12SPH5C"
+          applicationId="sandbox-sq0idb-nP6eTBYzaPexcBtq9o9l8Q"
+          locationId="LEWP2HC84SHQT"
+          cardTokenizeResponseReceived={async (token, buyer) => {
+            setOpenModal(true);
+            setLoading(true);
+
+            // let amount = flag ? 8 : count * 50;
+            let amount = flag ? 1 : count * 1;
+
+            const response = await axios.post(SERVER_URL + "/payment", {
+              sourceId: token.token,
+              amount: amount,
+            });
+
+            if (response.data.data.payment.status === "COMPLETED") {
+              if (flag) {
+                const res = await axios.post(SERVER_URL + "/single", {
+                  company: company,
+                  address: address,
+                  name: name,
+                  email: email,
+                  link: link,
+                  size: size,
+                });
+                if (res.data.success) {
+                  setLoading(false);
+                  toast.success(res.data.message);
+
+                  dispatch(
+                    singleActions.setSingle({
+                      flag: false,
+                      company: "",
+                      address: "",
+                      name: "",
+                      email: "",
+                      link: "",
+                      size: "",
+                    })
+                  );
+                  router("/singlereceipt");
+                } else {
+                  toast.error(res.data.message);
+                }
+              } else {
+                const res = await axios.post(SERVER_URL + "/activate", {
+                  userId: currentUser.id,
+                  amount: amount,
+                });
+                if (res.data.success) {
+                  toast.success(res.data.message);
+                  localStorage.setItem("token", res.data.token);
+                  router("/generator");
+                } else {
+                  toast.error(res.data.message);
+                }
               }
             } else {
-              const res = await axios.post(SERVER_URL + "/activate", {
-                userId: currentUser.id,
-                amount: amount,
-              });
-              if (res.data.success) {
-                toast.success(res.data.message)
-                localStorage.setItem("token", res.data.token);
-                router("/generator")
-              }
-              else {
-                toast.error(res.data.message)
-              }
+              toast.error(response.data.data.body);
             }
-          }
-          else {
-            toast.error(response.data.data.body);
-          }
-        }}
+          }}
+        >
+          <CreditCard />
+        </PaymentForm>
+      </form>
+      <Modal
+        open={openModal}
+        onCancel={() => setOpenModal(false)}
+        footer={null}
+        className="custom-modal"
       >
-        <CreditCard />
-      </PaymentForm>
-    </form>
+        <Styled.ModalLayout>
+          {loading ? (
+            <Styled.Image src="assets/loading.webp" alt="No loading" />
+          ) :  (
+            <Styled.Single>
+              <Styled.Title> {flag ? "Order Completed" : "Active Completed"} </Styled.Title>
+              <Styled.Container>
+                <Styled.Row>
+                  <Styled.Header>Email:</Styled.Header>
+                  <Styled.Detail>innocentdev0512@gmail.com</Styled.Detail>
+                </Styled.Row>
+                <Styled.Row>
+                  <Styled.Header>Date:</Styled.Header>
+                  <Styled.Detail>2024/03/21</Styled.Detail>
+                </Styled.Row>
+                <Styled.Row>
+                  <Styled.Header>Amount:</Styled.Header>
+                  <Styled.Detail><span>100</span>USD</Styled.Detail>
+                </Styled.Row>
+                <Styled.Row>
+                  <Styled.Header>OrderID:</Styled.Header>
+                  <Styled.Detail>YLBd742Kn3QimiVodPFqvCJCLVXZY</Styled.Detail>
+                </Styled.Row>
+                <Styled.Row>
+                  <Styled.Link href="https://discord.com/channels/@me/1199859109076414615" target="_blank">View Transaction</Styled.Link>
+                </Styled.Row>
+              </Styled.Container>
+            </Styled.Single>
+          )}
+        </Styled.ModalLayout>
+      </Modal>
+    </>
   );
 };
 
